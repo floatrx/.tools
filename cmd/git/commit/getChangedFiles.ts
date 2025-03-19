06d28f9b35d6be19fcba5bb69521e4445a7b2c8f
Andrew floatrx <ifloatrx@gmail.com>
2025-03-20 01:16:31 +0200

    improvement: cmd/git/commit; add excludeShared to reduce code repetition

--- cmd/git/commit/getChangedFiles.ts
+++ cmd/git/commit/getChangedFiles.ts
@@ -1,14 +1,16 @@
 import { $ } from '@/process';
 import { resolvedPath } from '@/config/const';
 
+const excludeShared = (output: string) => output.split('\n').filter((file) => file && !file.startsWith('src/shared-'));
+
 export const getChangedFiles = async () => {
   const { stdout: diffOutput } = await $('git diff --name-only', resolvedPath.root);
   const { stdout: untrackedOutput } = await $('git ls-files --others --exclude-standard', resolvedPath.root);
   const { stdout: stagedOutput } = await $('git diff --cached --name-only', resolvedPath.root);
 
-  const changedFiles = diffOutput.split('\n').filter((file) => file && !file.startsWith('src/shared-'));
-  const untrackedFiles = untrackedOutput.split('\n').filter((file) => file && !file.startsWith('src/shared-'));
-  const stagedFiles = stagedOutput.split('\n').filter((file) => file && !file.startsWith('src/shared-'));
+  const changedFiles = excludeShared(diffOutput);
+  const untrackedFiles = excludeShared(untrackedOutput);
+  const stagedFiles = excludeShared(stagedOutput);
 
   const allFiles = [...new Set([...changedFiles, ...untrackedFiles, ...stagedFiles])];
 
